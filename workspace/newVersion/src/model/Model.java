fbce73a7bbec999157c8806c7174a35fe4c451eb
RTaylor121 <spud_t_92@hotmail.co.uk>
2013-08-14 13:11:15 +0100

    broke 3Windows eclipse project, newVersion is current. will delete 3Windows if all is well with newVersion. changed list highlighting colours. added menus to pathButtons. improved list highlighting. started work on controller section of code for displaying id selections on pathway

---
+++ workspace/newVersion/src/model/Model.java
@@ -0,0 +1,108 @@
+package model;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import peakml.IPeak;
+import peakml.IPeakSet;
+
+public class Model {
+		
+	static IdentificationStore idStore;
+	private static PeakStore peakStore;
+	static ArrayList<Link[]> linkingData;
+	boolean pathLoaded, idsLoaded;
+	
+	int[] selectedPeaks, selectedIds, linkedPeaks, linkedIds;
+	
+	ArrayList<Integer> linkIdRows;
+	
+	public Model(){
+		idStore = new IdentificationStore();
+		peakStore = new PeakStore();
+		pathLoaded = false;
+	}
+	
+	public properIdent[] loadIdentifications(File file){
+		return idStore.parseInputFile(file);
+	}
+	
+	public IdentificationStore getIdStore() {
+		return idStore;
+	}
+
+	public static void setIdStore(IdentificationStore idStore) {
+		Model.idStore = idStore;
+	}
+
+	public IPeakSet<IPeak> loadPeaks(File file){
+		return getPeakStore().parseInputFile(file);
+	}
+
+	public static void setPeakStore(PeakStore peakStore) {
+		Model.peakStore = peakStore;
+	}
+
+	public PeakStore getPeakStore() {
+		return peakStore;
+	}
+
+	public boolean isPathLoaded() {
+		return pathLoaded;
+	}
+
+	public void setPathLoaded(boolean pathLoaded) {
+		this.pathLoaded = pathLoaded;
+	}
+
+	public boolean isIdsLoaded() {
+		return idsLoaded;
+	}
+
+	public void setIdsLoaded(boolean idsLoaded) {
+		this.idsLoaded = idsLoaded;
+	}
+
+	public int[] getSelectedPeaks() {
+		return selectedPeaks;
+	}
+
+	public void setSelectedPeaks(int[] selectedPeaks) {
+		this.selectedPeaks = selectedPeaks;
+	}
+
+	public int[] getSelectedIds() {
+		return selectedIds;
+	}
+
+	public void setSelectedIds(int[] selectedIds) {
+		this.selectedIds = selectedIds;
+	}
+
+	public int[] getLinkedPeaks() {
+		return linkedPeaks;
+	}
+
+	public void setLinkedPeaks(int[] linkedPeaks) {
+		this.linkedPeaks = linkedPeaks;
+	}
+
+	public int[] getLinkedIds() {
+		return linkedIds;
+	}
+
+	public void setLinkedIds(int[] linkedIds) {
+		this.linkedIds = linkedIds;
+	}
+
+	public ArrayList<Integer> getLinkIdRows() {
+		return linkIdRows;
+	}
+
+	public void setLinkIdRows(ArrayList<Integer> linkIdRows) {
+		this.linkIdRows = linkIdRows;
+	}
+	
+	
+	
+}
